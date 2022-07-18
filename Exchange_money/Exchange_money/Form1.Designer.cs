c5d9a61888a6d2a56167d6fb1ceaca422827b46d
Lang Lang <sivlangsay38@gmail.com>
2022-07-18 15:56:58 +0700

    update

---
+++ Exchange_money/Exchange_money/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace Exchange_money
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtconsumer = new System.Windows.Forms.TextBox();
+            this.txtold = new System.Windows.Forms.TextBox();
+            this.txtnew = new System.Windows.Forms.TextBox();
+            this.txtconsumtion = new System.Windows.Forms.TextBox();
+            this.txtamount = new System.Windows.Forms.TextBox();
+            this.btncalculate = new System.Windows.Forms.Button();
+            this.btnremove = new System.Windows.Forms.Button();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Exchange_money.Properties.Resources.download__5_;
+            this.pictureBox1.Location = new System.Drawing.Point(57, 18);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(700, 211);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(67, 255);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Consumer ID :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(88, 308);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Old Meter :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(81, 361);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 23);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "New Meter :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(74, 414);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 23);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Consumtion :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(61, 467);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(130, 23);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Total Amount :";
+            // 
+            // txtconsumer
+            // 
+            this.txtconsumer.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtconsumer.Location = new System.Drawing.Point(229, 248);
+            this.txtconsumer.Name = "txtconsumer";
+            this.txtconsumer.Size = new System.Drawing.Size(384, 30);
+            this.txtconsumer.TabIndex = 6;
+            // 
+            // txtold
+            // 
+            this.txtold.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtold.Location = new System.Drawing.Point(229, 301);
+            this.txtold.Name = "txtold";
+            this.txtold.Size = new System.Drawing.Size(384, 30);
+            this.txtold.TabIndex = 7;
+            // 
+            // txtnew
+            // 
+            this.txtnew.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnew.Location = new System.Drawing.Point(229, 354);
+            this.txtnew.Name = "txtnew";
+            this.txtnew.Size = new System.Drawing.Size(384, 30);
+            this.txtnew.TabIndex = 8;
+            // 
+            // txtconsumtion
+            // 
+            this.txtconsumtion.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtconsumtion.Location = new System.Drawing.Point(229, 407);
+            this.txtconsumtion.Name = "txtconsumtion";
+            this.txtconsumtion.Size = new System.Drawing.Size(384, 30);
+            this.txtconsumtion.TabIndex = 9;
+            // 
+            // txtamount
+            // 
+            this.txtamount.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtamount.Location = new System.Drawing.Point(229, 460);
+            this.txtamount.Name = "txtamount";
+            this.txtamount.Size = new System.Drawing.Size(384, 30);
+            this.txtamount.TabIndex = 10;
+            // 
+            // btncalculate
+            // 
+            this.btncalculate.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalculate.Location = new System.Drawing.Point(47, 516);
+            this.btncalculate.Name = "btncalculate";
+            this.btncalculate.Size = new System.Drawing.Size(127, 52);
+            this.btncalculate.TabIndex = 11;
+            this.btncalculate.Text = "&Calculate";
+            this.btncalculate.UseVisualStyleBackColor = true;
+            this.btncalculate.Click += new System.EventHandler(this.btncalculate_Click);
+            // 
+            // btnremove
+            // 
+            this.btnremove.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnremove.Location = new System.Drawing.Point(210, 556);
+            this.btnremove.Name = "btnremove";
+            this.btnremove.Size = new System.Drawing.Size(127, 52);
+            this.btnremove.TabIndex = 12;
+            this.btnremove.Text = "&Remove";
+            this.btnremove.UseVisualStyleBackColor = true;
+            // 
+            // btnclear
+            // 
+            this.btnclear.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclear.Location = new System.Drawing.Point(47, 608);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(127, 52);
+            this.btnclear.TabIndex = 13;
+            this.btnclear.Text = "&Clear";
+            this.btnclear.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(361, 516);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(422, 148);
+            this.listBox1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 672);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.btnremove);
+            this.Controls.Add(this.btncalculate);
+            this.Controls.Add(this.txtamount);
+            this.Controls.Add(this.txtconsumtion);
+            this.Controls.Add(this.txtnew);
+            this.Controls.Add(this.txtold);
+            this.Controls.Add(this.txtconsumer);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtconsumer;
+        private System.Windows.Forms.TextBox txtold;
+        private System.Windows.Forms.TextBox txtnew;
+        private System.Windows.Forms.TextBox txtconsumtion;
+        private System.Windows.Forms.TextBox txtamount;
+        private System.Windows.Forms.Button btncalculate;
+        private System.Windows.Forms.Button btnremove;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Exchange_money/Exchange_money/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exchange_money
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncalculate_Click(object sender, EventArgs e)
+        {
+            //Declare
+            float total, kilowat;
+            //Input
+            //price = 500;
+            // kilowat = float.Parse(txtnew.Text) - float.Parse(txtold.Text);
+            kilowat = CalculatekM(float.Parse(txtnew.Text), float.Parse(txtold.Text));
+            //Process
+            //total = price * kilowat;
+            total = Calculatetotal(kilowat);
+            //Output
+            txtconsumtion.Text = kilowat.ToString() + "   KW";
+            txtamount.Text = total+ "  Riels";
+
+        }
+        private float CalculatekM(float NewMeter, float OldMeter)
+        {
+            return NewMeter - OldMeter;
+        }
+        private float Calculatetotal(float kilowat)
+        {
+            float price = 0.0f, total = 0.0f, grandtotal = 0.0f;
+            while (kilowat > 0)
+            {
+                if (kilowat > 1000)
+                {
+                    price = 1300; total = (kilowat - 1000) * price; kilowat = kilowat - (kilowat - 1000); listBox1.Items.Add(total);
+                }
+                else if (kilowat > 500)
+                {
+                    price = 1100; total = (kilowat - 500) * price; kilowat = kilowat - (kilowat - 500); listBox1.Items.Add(total);
+                }
+                else if (kilowat > 200)
+                {
+                    price = 900; total = (kilowat - 200) * price; kilowat = kilowat - (kilowat - 200); listBox1.Items.Add(total);
+                }
+                else if (kilowat > 100)
+                {
+                    price = 750; total = (kilowat - 100) * price; kilowat = kilowat - (kilowat - 100); listBox1.Items.Add(total);
+                }
+                else if (kilowat > 50)
+                {
+                    price = 550; total = (kilowat - 50) * price; kilowat = kilowat - (kilowat - 50); listBox1.Items.Add(total);
+                }
+                else
+                {
+                    price = 350; total = kilowat * price; kilowat = 0;
+                }
+                grandtotal += total;
+            }
+            return grandtotal;
+
+        }
+    }
+}
+
